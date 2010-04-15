d1c59b1b105732517b56e836f464a104aa5050db
Cubox <cubox@batman.local>
2010-04-14 21:20:45 -0300

    Implemented basic charts by trips

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,24 +36,61 @@ class UsersController < ApplicationController
     colors_legend = []
     miles = []
     days = []
+    data = []
 
     Mode.all.each do |mode|
       legend << mode.name
       colors_legend << rand(0xffffff).to_s(16)
+      #data << @user.trips.find_all_by_mode_id(mode.id).map(&:distance)
     end
 
+    miles = [0]
     @user.trips.all.each do |trip|
       miles << trip.distance
       days << trip.made_at
     end
 
+    miles = miles.uniq.sort
+    days = days.uniq.sort
+    
+    days.each do |time|
+      new_trip = []
+      Mode.all.each do |mode|
+        trip = @user.trips.find(:first, :conditions => ["mode_id = ? AND made_at = ?", mode.id, time])
+        trip = (trip != nil) ? trip.distance : 0
+        #trip = trip.distance unless trip.nil?
+        new_trip << trip
+      end
+      data << new_trip
+    end
+    
+    data_graph = []
+    data.each do |new_data|
+      cont = 0
+      new_data.each do |mile|
+        if data_graph[cont].nil? 
+          data_graph[cont] = [0]
+        end
+        posc = miles.index(mile)
+       
+        if posc!=0 && !posc.nil?
+          data_graph[cont] << (posc * 100) / (miles.length - 1)
+        end
+      
+        cont = cont + 1
+      end
+    end
+   
+    debugger
+
     @image = Gchart.line(
-      :title => "Testing",
       :legend => legend,
       :bar_colors => colors_legend,
-      :data => [[1, 5, 3, 6, 9, 7, 10], [3,5, 9, 10]],
+      #:data => [0, 50, 100],
+      :data => data_graph,
       :axis_with_labels => 'x,y',
-      :axis_labels => [days, miles]
+      :axis_labels => [days, miles],
+      :max_value => 100
     )
   end
  
